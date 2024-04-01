f0d7541ca67dac665f50473afe358db044ab98f5
IronKim <tankokim@gmail.com>
2024-04-01 17:35:52 +0900

    SecurityConfig설정
    인증방식으로 JWT를 채택했다.
    build.gradle에 JWT라이브러리를 추가하고
    application.yml에 JWTSecretKey와 유효시간을 추가해주었다.

---
+++ src/main/java/com/ironkim/moyeobang/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ironkim.moyeobang.config;
+
+import com.ironkim.moyeobang.config.filter.JwtTokenFilter;
+import com.ironkim.moyeobang.exception.CustomAuthenticationEntryPoint;
+import com.ironkim.moyeobang.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(RegexRequestMatcher.regexMatcher("^(?!/api/).*"))
+                .requestMatchers("/api/*/auth/*");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthService authService) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/api/*/auth/*").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenFilter(key, authService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(authenticationManager -> authenticationManager.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/ironkim/moyeobang/util/JwtTokenUtils.java
@@ -0,0 +1,42 @@
+package com.ironkim.moyeobang.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtTokenUtils {
+    public static String getAccountId(String token, String key) {
+        return extractClaims(token, key).get("AccountId", String.class);
+    }
+
+    public static boolean isExpired(String token, String key) {
+        Date expiredDate = extractClaims(token, key).getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+    private static Claims extractClaims(String token, String key) {
+        return Jwts.parserBuilder().setSigningKey(getKey(key)).build().parseClaimsJws(token).getBody();
+    }
+
+    public static String generateToken(String userName, String key, long expiredTimeMs) {
+        Claims claims = Jwts.claims();
+        claims.put("AccountId", userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                .signWith(getKey(key), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private static Key getKey(String key) { // 키를 Key 객체로 변환하는 메서드
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8); // 키를 바이트 배열로 변환
+        return Keys.hmacShaKeyFor(keyBytes); // 바이트 배열로 변환된 키를 Key 객체로 변환
+    }
+}
