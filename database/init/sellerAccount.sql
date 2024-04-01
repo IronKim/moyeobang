d4717df7329945bdb1a18232a6fb337b27a8ffe2
IronKim <tankokim@gmail.com>
2024-04-01 17:04:57 +0900

    SellerAccount 도메인과 테이블 생성

---
+++ database/init/sellerAccount.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `seller_account`;
+
+CREATE TABLE `seller_account` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(20) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `birthday` date NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role_type` varchar(20) NOT NULL,
+  `seller_name` varchar(20) NOT NULL,
+  `business_number` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(20) NOT NULL,
+  `modified_at` datetime NOT NULL,
+  `modified_by` varchar(20) NOT NULL,
+  `deleted_at` datetime NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
